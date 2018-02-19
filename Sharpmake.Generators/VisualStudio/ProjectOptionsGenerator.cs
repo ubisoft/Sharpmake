51c444cb73be7fdae1fd9530ce7fabe46933df38
Benoit Rousseau <benoit.rousseau@ubisoft.com>
2018-02-19 13:08:53 -0500

    Fixes KeepComment compiler option.
    * Fixes a problem where the PreprocessKeepComment of generated vcxproj was set to the value of KeepComments instead.
    * Fixes a problem where the PreprocessSuppressLineNumbers of generated vcxproj is always false.

--- Sharpmake.Generators/VisualStudio/ProjectOptionsGenerator.cs
+++ Sharpmake.Generators/VisualStudio/ProjectOptionsGenerator.cs
@@ -490,9 +490,9 @@ namespace Sharpmake.Generators.VisualStudio
             //    WithoutLineNumbers                      GeneratePreprocessedFile="2"                    /EP /P
             context.SelectOption
             (
-            Options.Option(Options.Vc.Compiler.GenerateProcessorFile.Disable, () => { context.Options["GeneratePreprocessedFile"] = "false"; context.CommandLineOptions["GeneratePreprocessedFile"] = FileGeneratorUtilities.RemoveLineTag; }),
-            Options.Option(Options.Vc.Compiler.GenerateProcessorFile.WithLineNumbers, () => { context.Options["GeneratePreprocessedFile"] = "true"; context.CommandLineOptions["GeneratePreprocessedFile"] = "/P"; }),
-            Options.Option(Options.Vc.Compiler.GenerateProcessorFile.WithoutLineNumbers, () => { context.Options["GeneratePreprocessedFile"] = "true"; context.CommandLineOptions["GeneratePreprocessedFile"] = "/EP /P"; })
+            Options.Option(Options.Vc.Compiler.GenerateProcessorFile.Disable, () => { context.Options["GeneratePreprocessedFile"] = "false"; context.Options["PreprocessSuppressLineNumbers"] = "false"; context.CommandLineOptions["GeneratePreprocessedFile"] = FileGeneratorUtilities.RemoveLineTag; }),
+            Options.Option(Options.Vc.Compiler.GenerateProcessorFile.WithLineNumbers, () => { context.Options["GeneratePreprocessedFile"] = "true"; context.Options["PreprocessSuppressLineNumbers"] = "false"; context.CommandLineOptions["GeneratePreprocessedFile"] = "/P"; }),
+            Options.Option(Options.Vc.Compiler.GenerateProcessorFile.WithoutLineNumbers, () => { context.Options["GeneratePreprocessedFile"] = "true"; context.Options["PreprocessSuppressLineNumbers"] = "true"; context.CommandLineOptions["GeneratePreprocessedFile"] = "/EP /P"; })
             );
 
             //Options.Vc.Compiler.KeepComment.
