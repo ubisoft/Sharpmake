7888b81f1beb78ba9b49aa43b68a331615cdab98
Lambert Clara <lambert.clara@ubisoft.com>
2018-12-25 18:13:15 +0100

    - Fix error handling in bootstrap, the exit that were called from functions didn't exit the bat - Add a timeout when it succeeds

--- bootstrap-sharpmake.bat
+++ bootstrap-sharpmake.bat
@@ -12,29 +12,28 @@ if not defined VS140COMNTOOLS (
 )
 
 call "%VS140COMNTOOLS%VsMSBuildCmd.bat"
+if %errorlevel% NEQ 0 goto error
 
 call :NugetRestore Sharpmake/Sharpmake.csproj
+if %errorlevel% NEQ 0 goto error
+
 call :BuildCsproj Sharpmake.Application/Sharpmake.Application.csproj Debug AnyCPU
+if %errorlevel% NEQ 0 goto error
 
 set SM_CMD=%SHARPMAKE_EXECUTABLE% /sources("Sharpmake.Main.sharpmake.cs") /verbose
-
 echo %SM_CMD%
 %SM_CMD%
+if %errorlevel% NEQ 0 goto error
 
-if %errorlevel% NEQ 0 (
-    goto error
-)
-
-echo Sharpmake solution generated.
-goto end
+goto success
 
 @REM -----------------------------------------------------------------------
 :NugetRestore
 echo Restoring nuget packages for %~1
 dotnet restore "%~1"
-if errorlevel 1 (
+if %errorlevel% NEQ 0 (
     echo ERROR: Failed to restore nuget package for %~1
-    goto error
+    exit /b 1
 )
 exit /b 0
 
@@ -43,23 +42,29 @@ exit /b 0
 echo Compiling %~1 in "%~2|%~3"...
 
 msbuild /nologo /verbosity:quiet /p:Configuration="%~2" /p:Platform="%~3" "%~1"
-if errorlevel 1 (
+if %errorlevel% NEQ 0 (
     echo ERROR: Failed to compile %~1 in "%~2|%~3".
-    goto error
+    exit /b 1
 )
 exit /b 0
+
+@REM -----------------------------------------------------------------------
+:success
+COLOR 2F
+echo Boostrap succeeded!
+timeout /t 5
+exit /b 0
+
+@REM -----------------------------------------------------------------------
+:error
+COLOR 4F
+echo Boostrap failed!
+pause
+set ERROR_CODE=1
+goto end
 
 @REM -----------------------------------------------------------------------
 :end
 :: restore caller current directory
 popd
-COLOR
-exit /b 0
-
-@REM -----------------------------------------------------------------------
-:error
-:: restore caller current directory
-popd
-COLOR 4F
-pause
-exit /b 1
+exit /b %ERROR_CODE%
