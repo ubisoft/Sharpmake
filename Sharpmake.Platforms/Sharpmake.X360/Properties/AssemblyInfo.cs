0f2e7ecc47aee1b3afb1827416f7c3f7869b9500
Lambert Clara <lambert.clara@ubisoft.com>
2019-01-24 11:46:02 +0100

    Bump to 0.8.0

--- Sharpmake.Platforms/Sharpmake.X360/Properties/AssemblyInfo.cs
+++ Sharpmake.Platforms/Sharpmake.X360/Properties/AssemblyInfo.cs
@@ -44,8 +44,6 @@ using Sharpmake;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.8.0.0")]
 
 [assembly: SharpmakeExtension]
