fd0228fa7f960880a41fe37ecbf38d3e030d2e25
Lambert Clara <lambert.clara@ubisoft.com>
2019-06-28 15:01:19 +0200

    Version 0.10.1

--- Sharpmake.Platforms/Sharpmake.NvShield/Properties/AssemblyInfo.cs
+++ Sharpmake.Platforms/Sharpmake.NvShield/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using Sharpmake;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.10.1.*")]
+[assembly: AssemblyVersion("0.10.1.0")]
 
 [assembly: SharpmakeExtension]
--- Sharpmake/Properties/AssemblyInfo.cs
+++ Sharpmake/Properties/AssemblyInfo.cs
@@ -44,7 +44,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.10.1.*")]
+[assembly: AssemblyVersion("0.10.1.0")]
 #pragma warning disable CS7035
 [assembly: AssemblyFileVersion("0.10.1.0 (LocalBuild)")]
 #pragma warning restore 
