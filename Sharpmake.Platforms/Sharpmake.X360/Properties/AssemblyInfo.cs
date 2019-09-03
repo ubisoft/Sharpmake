9bb84b2a6322c4c31b4bc8086a37c3f9a9e81bf7
Jean-Sebastien Pelletier <jean-sebastien.pelletier@ubisoft.com>
2019-09-03 08:54:46 -0400

    Start dev of 0.10.7.*

--- Sharpmake.Platforms/Sharpmake.X360/Properties/AssemblyInfo.cs
+++ Sharpmake.Platforms/Sharpmake.X360/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using Sharpmake;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.10.6.0")]
+[assembly: AssemblyVersion("0.10.7.*")]
 
 [assembly: SharpmakeExtension]
--- Sharpmake/Properties/AssemblyInfo.cs
+++ Sharpmake/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.10.6.0")]
+[assembly: AssemblyVersion("0.10.7.*")]
 #pragma warning disable CS7035
-[assembly: AssemblyFileVersion("0.10.6.* (LocalBuild)")]
+[assembly: AssemblyFileVersion("0.10.7.* (LocalBuild)")]
 #pragma warning restore
 
 [assembly: InternalsVisibleTo("Sharpmake.Application")]
