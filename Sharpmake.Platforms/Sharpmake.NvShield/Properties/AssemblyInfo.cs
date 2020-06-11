2e6476856515e1055b6cb7dd6a8cf0cd998c24a6
Lambert Clara <lambert.clara@ubisoft.com>
2020-06-11 17:02:00 +0200

    Make the version unofficial.

--- Sharpmake.Platforms/Sharpmake.NvShield/Properties/AssemblyInfo.cs
+++ Sharpmake.Platforms/Sharpmake.NvShield/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using Sharpmake;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.14.0.0")]
+[assembly: AssemblyVersion("0.14.0.*")]
 
 [assembly: SharpmakeExtension]
