da39329cc17767a425d869a35d2f1116765860c7
Guillaume BUCHLE <guillaume.buchle@ubisoft.com>
2020-08-07 13:54:52 -0400

    [XCode] Sort source files and ressource files
    remove the AssetCatalog XCode option, while it is used like any other ressource (use directly project.ResourceFiles)

--- Sharpmake/Options.XCode.cs
+++ Sharpmake/Options.XCode.cs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+using System;
 using System.Linq;
 
 namespace Sharpmake
@@ -186,6 +187,7 @@ namespace Sharpmake
                     { }
                 }
 
+                [Obsolete("AssetCatalog is not used anymore.", error: true)]
                 public class AssetCatalog : Strings
                 {
                     public AssetCatalog(params string[] paths)
