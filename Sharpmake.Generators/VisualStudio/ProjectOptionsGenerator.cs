1e1851fb5fa682a9e47740c1ee1337252ae1f1c1
Chris Savoie <chris@chrissavoie.com>
2020-10-19 14:47:28 +0200

    Cleanup useless using

--- Sharpmake.Generators/VisualStudio/ProjectOptionsGenerator.cs
+++ Sharpmake.Generators/VisualStudio/ProjectOptionsGenerator.cs
@@ -13,7 +13,6 @@
 // limitations under the License.
 using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.IO;
 using System.Linq;
 using System.Text;
