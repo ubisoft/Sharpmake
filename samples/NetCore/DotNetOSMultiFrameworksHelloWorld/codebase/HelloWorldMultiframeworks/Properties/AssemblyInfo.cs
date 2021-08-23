d9021e4ecd6fd059d986aaf2d79974a566dfb976
Lambert Clara <lambert.clara@ubisoft.com>
2021-08-23 09:56:22 +0200

    Update copyrights

--- samples/NetCore/DotNetOSMultiFrameworksHelloWorld/codebase/HelloWorldMultiframeworks/Properties/AssemblyInfo.cs
+++ samples/NetCore/DotNetOSMultiFrameworksHelloWorld/codebase/HelloWorldMultiframeworks/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ubisoft")]
 [assembly: AssemblyProduct("Sharpmake")]
-[assembly: AssemblyCopyright("Copyright © Ubisoft 2020")]
+[assembly: AssemblyCopyright("Copyright © Ubisoft 2021")]
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
