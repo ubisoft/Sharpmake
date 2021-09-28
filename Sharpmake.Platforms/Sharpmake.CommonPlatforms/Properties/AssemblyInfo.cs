7ca150ab86c8ecafc8a2135c6da303789f5d07e6
Lambert Clara <lambert.clara@ubisoft.com>
2021-09-28 10:00:19 +0200

    Bump version number to 0.17.3

--- Sharpmake.Platforms/Sharpmake.CommonPlatforms/Properties/AssemblyInfo.cs
+++ Sharpmake.Platforms/Sharpmake.CommonPlatforms/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using Sharpmake;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.17.2.*")]
+[assembly: AssemblyVersion("0.17.3.0")]
 
 [assembly: SharpmakeExtension]
--- Sharpmake/Properties/AssemblyInfo.cs
+++ Sharpmake/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.17.2.*")]
+[assembly: AssemblyVersion("0.17.3.0")]
 #pragma warning disable CS7035
-[assembly: AssemblyFileVersion("0.17.2.* (LocalBuild)")]
+[assembly: AssemblyFileVersion("0.17.3.0 (LocalBuild)")]
 #pragma warning restore
 
 [assembly: InternalsVisibleTo("Sharpmake.Application")]
