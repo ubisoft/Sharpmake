b32686dd173fc8dd4d236a5857be086781ef1d0c
Lambert Clara <lambert.clara@ubisoft.com>
2021-10-06 10:28:36 +0200

    Make the version unofficial.

--- Sharpmake.Platforms/Sharpmake.CommonPlatforms/Properties/AssemblyInfo.cs
+++ Sharpmake.Platforms/Sharpmake.CommonPlatforms/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using Sharpmake;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.17.5.0")]
+[assembly: AssemblyVersion("0.17.5.*")]
 
 [assembly: SharpmakeExtension]
