4ff515baa54ab63815cfd02ffbcd84ec55496039
Lambert Clara <lambert.clara@ubisoft.com>
2021-11-15 13:23:10 +0100

    Add support for .NET 6

--- Sharpmake/ExtensionMethods.cs
+++ Sharpmake/ExtensionMethods.cs
@@ -1,11 +1,11 @@
 // Copyright (c) 2017-2021 Ubisoft Entertainment
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -94,6 +94,8 @@ namespace Sharpmake
                     return "netstandard2.1";
                 case DotNetFramework.net5_0:
                     return "net5.0";
+                case DotNetFramework.net6_0:
+                    return "net6.0";
                 case DotNetFramework.all_netframework:
                 case DotNetFramework.all_netcore:
                 case DotNetFramework.all_netstandard:
@@ -141,6 +143,8 @@ namespace Sharpmake
                     return "netcoreapp3.1";
                 case DotNetFramework.net5_0:
                     return "net5.0";
+                case DotNetFramework.net6_0:
+                    return "net6.0";
                 case DotNetFramework.netstandard1_0:
                     return "netstandard1.0";
                 case DotNetFramework.netstandard1_1:
--- Sharpmake/Target.cs
+++ Sharpmake/Target.cs
@@ -162,22 +162,24 @@ namespace Sharpmake
         netcore2_2 = 1 << 14,
         netcore3_0 = 1 << 15,
         netcore3_1 = 1 << 16,
-        net5_0 = 1 << 17,
 
-        netstandard1_0 = 1 << 18,
-        netstandard1_1 = 1 << 19,
-        netstandard1_2 = 1 << 20,
-        netstandard1_3 = 1 << 21,
-        netstandard1_4 = 1 << 22,
-        netstandard1_5 = 1 << 23,
-        netstandard1_6 = 1 << 24,
-        netstandard2_0 = 1 << 25,
-        netstandard2_1 = 1 << 26,
+        net5_0 = 1 << 17,
+        net6_0 = 1 << 18,
+
+        netstandard1_0 = 1 << 19,
+        netstandard1_1 = 1 << 20,
+        netstandard1_2 = 1 << 21,
+        netstandard1_3 = 1 << 22,
+        netstandard1_4 = 1 << 23,
+        netstandard1_5 = 1 << 24,
+        netstandard1_6 = 1 << 25,
+        netstandard2_0 = 1 << 26,
+        netstandard2_1 = 1 << 27,
 
         [CompositeFragment]
         all_netframework = v3_5 | v3_5clientprofile | v4_5_2 | v4_6 | v4_6_1 | v4_6_2 | v4_7 | v4_7_1 | v4_7_2 | v4_8,
         [CompositeFragment]
-        all_netcore = netcore1_0 | netcore1_1 | netcore2_0 | netcore2_1 | netcore3_0 | netcore3_1 | net5_0,
+        all_netcore = netcore1_0 | netcore1_1 | netcore2_0 | netcore2_1 | netcore3_0 | netcore3_1 | net5_0 | net6_0,
         [CompositeFragment]
         all_netstandard = netstandard1_0 | netstandard1_1 | netstandard1_2 | netstandard1_3 | netstandard1_4 | netstandard1_5 | netstandard1_6 | netstandard2_0 | netstandard2_1,
 
