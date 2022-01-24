e4da5b5598d5b948f23115943e6d99db95cd73b8
Lambert Clara <lambert.clara@ubisoft.com>
2022-01-24 15:26:54 +0100

    Bump version number to 0.18.3

--- Sharpmake.Platforms/Sharpmake.CommonPlatforms/Properties/AssemblyInfo.cs
+++ Sharpmake.Platforms/Sharpmake.CommonPlatforms/Properties/AssemblyInfo.cs
@@ -44,6 +44,6 @@ using Sharpmake;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.18.2.0")]
+[assembly: AssemblyVersion("0.18.3.0")]
 
 [assembly: SharpmakeExtension]
