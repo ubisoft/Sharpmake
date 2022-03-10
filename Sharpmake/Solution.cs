ddde62114f807b0312cb2f415e056df871661bc5
Lambert Clara <lambert.clara@ubisoft.com>
2022-03-10 18:40:17 +0100

    [FastBuildAll] Propagate the first project conf options to the FastBuildAll conf

--- Sharpmake/Solution.cs
+++ Sharpmake/Solution.cs
@@ -642,6 +642,7 @@ namespace Sharpmake
                     projectConf.FastBuildCustomArgs = firstProject.Configuration.FastBuildCustomArgs;
                     projectConf.FastBuildCustomActionsBeforeBuildCommand = firstProject.Configuration.FastBuildCustomActionsBeforeBuildCommand;
                     projectConf.FastBuildDistribution = firstProject.Configuration.FastBuildDistribution;
+                    projectConf.Options.AddRange(firstProject.Configuration.Options);
 
                     // add all the projects to build as private dependencies, and OnlyBuildOrder
                     foreach (Configuration.IncludedProjectInfo projectConfigToBuild in projectConfigsToBuild)
