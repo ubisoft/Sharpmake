001f502d28b61516731bdf7017f4acf3cc23e62c
Lambert Clara <lambert.clara@ubisoft.com>
2022-11-23 16:33:14 +0100

    Bump version number to 0.22.0

--- Sharpmake/Properties/AssemblyInfo.cs
+++ Sharpmake/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.21.1.0")]
+[assembly: AssemblyVersion("0.22.0.0")]
 #pragma warning disable CS7035
-[assembly: AssemblyFileVersion("0.21.1.0 (LocalBuild)")]
+[assembly: AssemblyFileVersion("0.22.0.0 (LocalBuild)")]
 #pragma warning restore
 
 [assembly: InternalsVisibleTo("Sharpmake.Application")]
