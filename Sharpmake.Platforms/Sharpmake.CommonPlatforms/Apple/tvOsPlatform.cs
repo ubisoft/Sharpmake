4e294e2c56c15d3da5491c27b6e1ed33be61b105
Chris Helmich <kagekirin@gmail.com>
2023-01-10 17:44:08 +0100

    Add new Apple Platform tvOsPlatform

---
+++ Sharpmake.Platforms/Sharpmake.CommonPlatforms/Apple/tvOsPlatform.cs
@@ -0,0 +1,106 @@
+// Copyright (c) 2020-2021 Ubisoft Entertainment
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Sharpmake.Generators;
+using Sharpmake.Generators.FastBuild;
+using Sharpmake.Generators.VisualStudio;
+
+namespace Sharpmake
+{
+    public static partial class Apple
+    {
+        [PlatformImplementation(Platform.tvos,
+            typeof(IPlatformDescriptor),
+            typeof(IFastBuildCompilerSettings),
+            typeof(IPlatformBff),
+            typeof(IClangPlatformBff),
+            typeof(IPlatformVcxproj),
+            typeof(Project.Configuration.IConfigurationTasks))]
+        public sealed partial class tvOsPlatform : BaseApplePlatform
+        {
+            public override Platform SharpmakePlatform => Platform.tvos;
+
+            #region IPlatformDescriptor implementation.
+            public override string SimplePlatformString => "tvOS";
+            #endregion
+
+            public override string BffPlatformDefine => "_TVOS";
+
+            public override string CConfigName(Configuration conf)
+            {
+                return ".tvosConfig";
+            }
+
+            public override string CppConfigName(Configuration conf)
+            {
+                return ".tvosppConfig";
+            }
+
+            protected override void WriteCompilerExtraOptionsGeneral(IFileGenerator generator)
+            {
+                base.WriteCompilerExtraOptionsGeneral(generator);
+                generator.Write(_compilerExtraOptionsGeneral);
+            }
+
+            public override void SelectCompilerOptions(IGenerationContext context)
+            {
+                base.SelectCompilerOptions(context);
+
+                var options = context.Options;
+                var cmdLineOptions = context.CommandLineOptions;
+                var conf = context.Configuration;
+
+                // Sysroot
+                options["SDKRoot"] = "appletvos";
+                cmdLineOptions["SDKRoot"] = $"-isysroot {XCodeDeveloperFolder}/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS.sdk";
+                Options.XCode.Compiler.SDKRoot customSdkRoot = Options.GetObject<Options.XCode.Compiler.SDKRoot>(conf);
+                if (customSdkRoot != null)
+                {
+                    options["SDKRoot"] = customSdkRoot.Value;
+                    cmdLineOptions["SDKRoot"] = $"-isysroot {customSdkRoot.Value}";
+                }
+
+                // Target
+                options["MacOSDeploymentTarget"] = FileGeneratorUtilities.RemoveLineTag;
+
+                Options.XCode.Compiler.IPhoneOSDeploymentTarget tvosDeploymentTarget = Options.GetObject<Options.XCode.Compiler.IPhoneOSDeploymentTarget>(conf);
+                if (tvosDeploymentTarget != null)
+                {
+                    options["IPhoneOSDeploymentTarget"] = tvosDeploymentTarget.MinimumVersion;
+                    cmdLineOptions["IPhoneOSDeploymentTarget"] = $"-target arm64-apple-tvos{tvosDeploymentTarget.MinimumVersion}";
+                }
+                else
+                {
+                    options["IPhoneOSDeploymentTarget"] = FileGeneratorUtilities.RemoveLineTag;
+                    cmdLineOptions["IPhoneOSDeploymentTarget"] = FileGeneratorUtilities.RemoveLineTag;
+                }
+            }
+
+            public override void SelectLinkerOptions(IGenerationContext context)
+            {
+                base.SelectLinkerOptions(context);
+
+                var options = context.Options;
+                var cmdLineOptions = context.CommandLineOptions;
+                var conf = context.Configuration;
+
+                // Sysroot
+                cmdLineOptions["SysLibRoot"] = $"-syslibroot {XCodeDeveloperFolder}/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS.sdk";
+                Options.XCode.Compiler.SDKRoot customSdkRoot = Options.GetObject<Options.XCode.Compiler.SDKRoot>(conf);
+                if (customSdkRoot != null)
+                    cmdLineOptions["SysLibRoot"] = $"-isysroot {customSdkRoot.Value}";
+            }
+        }
+    }
+}
