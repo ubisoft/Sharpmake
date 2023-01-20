dad1c43a95c209be5986f4eb3363ab8d4f4f2cf9
Juscelino Tanaka <juscelino.tanaka@ubisoft.com>
2023-01-20 15:39:44 +0100

    Bump version number to 0.23.0

--- Sharpmake.Generators/Properties/AssemblyInfo.cs
+++ Sharpmake.Generators/Properties/AssemblyInfo.cs
@@ -44,7 +44,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.22.2.0")]
+[assembly: AssemblyVersion("0.23.1.0")]
 
 [assembly: InternalsVisibleTo("Sharpmake")]
 [assembly: InternalsVisibleTo("Sharpmake.UnitTests")]
