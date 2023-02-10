537df6e019bb842680bc18699c77c3e6bd98f7e2
Fabien Bole-Feysot <fabien.bole-feysot@ubisoft.com>
2023-02-10 09:40:56 +0100

    set new copyright header for all files

--- Sharpmake.UnitTests/TestFileGenerator.cs
+++ Sharpmake.UnitTests/TestFileGenerator.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Ubisoft. All Rights Reserved.
+// Licensed under the Apache 2.0 License. See LICENSE.md in the project root for license information.
+
+using System;
 using System.IO;
 using System.Linq;
 using System.Text;
