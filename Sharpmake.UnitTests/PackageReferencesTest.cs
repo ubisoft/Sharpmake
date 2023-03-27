2c5caf7c0539f8e965b8768f04f79ef6f4535fc6
Jouni Pänkäläinen <jouni.pankalainen@remedygames.com>
2023-03-27 11:05:06 +0300

    Fixed unit test compilation

--- Sharpmake.UnitTests/PackageReferencesTest.cs
+++ Sharpmake.UnitTests/PackageReferencesTest.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Ubisoft. All Rights Reserved.
 // Licensed under the Apache 2.0 License. See LICENSE.md in the project root for license information.
 
+using System;
 using System.Linq;
 using NUnit.Framework;
 
