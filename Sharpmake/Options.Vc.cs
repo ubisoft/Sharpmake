bbdde4110f9775140207a9f4118de76e56ba436e
Marc-André Laperle <marc-andre.laperle@ubisoft.com>
2024-07-23 14:53:44 -0400

    Support for Windows SDK 10.0.26100

--- Sharpmake/Options.Vc.cs
+++ Sharpmake/Options.Vc.cs
@@ -76,6 +76,7 @@ namespace Sharpmake
                     v10_0_20348_0, // 2104/21H1
                     v10_0_22000_0, // Windows 11
                     v10_0_22621_0, // Windows 11 22H2
+                    v10_0_26100_0, // Windows 11 24H2
                     Latest,        // latest available in host machine
                 }
 
