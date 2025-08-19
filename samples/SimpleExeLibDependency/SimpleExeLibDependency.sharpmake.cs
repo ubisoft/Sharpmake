db1317479519b2ab8594a09d7bfc6f72a4c02ac6
Sylvain Fortin <sylvain.fortin@ubisoft.com>
2025-08-19 16:19:32 -0400

    Bump references in samples to older Windows SDK version 10.0.17763.0 to 10.0.22621.0
    Some CI environments have removed this SDK.

--- samples/SimpleExeLibDependency/SimpleExeLibDependency.sharpmake.cs
+++ samples/SimpleExeLibDependency/SimpleExeLibDependency.sharpmake.cs
@@ -60,7 +60,7 @@ namespace SimpleExeLibDependency
         [Sharpmake.Main]
         public static void SharpmakeMain(Sharpmake.Arguments arguments)
         {
-            KitsRootPaths.SetUseKitsRootForDevEnv(DevEnv.vs2022, KitsRootEnum.KitsRoot10, Options.Vc.General.WindowsTargetPlatformVersion.v10_0_17763_0);
+            KitsRootPaths.SetUseKitsRootForDevEnv(DevEnv.vs2022, KitsRootEnum.KitsRoot10, Options.Vc.General.WindowsTargetPlatformVersion.v10_0_22621_0);
 
             arguments.Generate<ExeLibSolution>();
         }
