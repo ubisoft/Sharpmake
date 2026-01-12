ba1026f9a9b420714c67cef3f3623cad2758d1ac
Martin Ecker <me@martinecker.com>
2026-01-12 13:10:47 -0800

    Trivial white spacing change in filegeneration.bat to match existing indentation (using tabs instead of spaces).

--- samples/CustomBuildStep/codebase/filegeneration.bat
+++ samples/CustomBuildStep/codebase/filegeneration.bat
@@ -3,7 +3,7 @@ SET FILENAME=%~dp0\main.cpp
 
 echo int main(int, char**) > %FILENAME%
 echo { >> %FILENAME%
-echo    extern void PrintConcatenatedFileContents(); >> %FILENAME%
-echo    PrintConcatenatedFileContents(); >> %FILENAME%
+echo 	extern void PrintConcatenatedFileContents(); >> %FILENAME%
+echo 	PrintConcatenatedFileContents(); >> %FILENAME%
 echo 	return 0; >> %FILENAME%
 echo } >> %FILENAME%
